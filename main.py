bd9e3ac9a67392413775dfa6739fba0f4601ad8d
Philipp <philipp@redefine.studio>
2024-03-15 15:56:04 +0100

    Started building

---
+++ main.py
@@ -0,0 +1,13 @@
+from photobooth import PhotoBooth
+
+
+def main():
+    # Initialize the photobooth
+    photo_booth = PhotoBooth()
+
+    # Start the application
+    photo_booth.start()
+
+
+if __name__ == "__main__":
+    main()
