ea997755561103b2a7e7964c3167645992c6f54a
Philipp <philipp@redefine.studio>
2025-05-09 16:56:31 +0200

    Update test

--- plotfile.py
+++ plotfile.py
@@ -1,11 +1,19 @@
 import os
 from nextdraw import NextDraw
 
+# File
+file_name = "photo-output-1.svg"
 
+# Construct the path
+base_path = os.path.dirname(os.path.abspath(__file__))
+base_dir = os.path.join(base_path, "photos", "current")
+file_path = os.path.join(base_dir, file_name)
+
+# Pass the constructed path
+print(file_path)
 nd1 = NextDraw()
-nd1.plot_setup("./photos/current/photo_output_0.svg")
+
+            
+nd1.plot_setup(file_path)
 nd1.plot_run()
-
-
-
 
