7a9ec8c52355c93ca9d70a3bafb956a26d5dcd4c
Philipp Lehmann <philipp@redefine.studio>
2025-05-19 21:07:06 +0200

    Moved utils in separate file

--- photobooth/imageparser.py
+++ photobooth/imageparser.py
@@ -1,7 +1,6 @@
 import os
 import re
 import random
-import time
 
 import cv2
 import dlib
@@ -11,26 +10,7 @@ import torch
 from torchvision import transforms
 from scipy.spatial import cKDTree
 from lxml import etree
-
-# ----- PROFILE / OPTIMIZATION -----
-def profile(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        duration = time.time() - start_time
-
-        # Bar settings
-        unit = 0.05  # seconds per '='
-        max_len = 20  # max number of '=' in bar
-        num_eq = min(int(duration / unit), max_len)
-        num_dash = max_len - num_eq
-
-        # ANSI color for yellow: \033[33m
-        bar = f"\033[33m{'=' * num_eq}\033[0m{'-' * num_dash}"
-
-        print(f"[{bar}] \033[1;33m⏲ {func.__name__}\033[0m took {duration:.2f} seconds")
-        return result
-    return wrapper
+from utils import profile
 
 class ImageParser:
     def __init__(self):
