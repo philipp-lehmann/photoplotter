cf7f7ece988a64fc6a85383ef25f982f3684fd65
Philipp <philipp@redefine.studio>
2025-05-20 15:33:58 +0200

    Updated file attribute

--- plotfile.py
+++ plotfile.py
@@ -8,18 +8,13 @@ plotter = Plotter()
 # Set up argument parsing
 parser = argparse.ArgumentParser(description="Plot an image using the Plotter.")
 parser.add_argument(
-    "--file", 
-    required=True, 
-    help="Path to the SVG file to be plotted."
+    "file", 
+    help="Absolute path to the SVG file to be plotted."
 )
 
 # Parse the arguments
 args = parser.parse_args()
-file_name = args.file
-
-# Paths
-current_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(current_dir, "photos", "current", file_name)
+file_path = args.file
 
 # Plot the image
 print(file_path)
