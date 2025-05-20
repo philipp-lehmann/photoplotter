6161e747b03fe3840f4fdb2656d3ae85ef7274f0
Philipp <philipp@redefine.studio>
2025-05-20 15:32:13 +0200

    Plotfile with param

--- plotfile.py
+++ plotfile.py
@@ -1,16 +1,26 @@
 import os
+import argparse
 from photobooth.plotter import Plotter
 
+# Initialize the plotter
 plotter = Plotter()
 
-# File
-file_name = "photo-output-3.svg"
+# Set up argument parsing
+parser = argparse.ArgumentParser(description="Plot an image using the Plotter.")
+parser.add_argument(
+    "--file", 
+    required=True, 
+    help="Path to the SVG file to be plotted."
+)
+
+# Parse the arguments
+args = parser.parse_args()
+file_name = args.file
 
 # Paths
 current_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(current_dir, "photos", "current", file_name)
-config_path = os.path.join(current_dir, "photobooth", "nextdraw_conf.py")
 
-# Pass the constructed path
+# Plot the image
 print(file_path)
 plotter.plot_image(file_path)
