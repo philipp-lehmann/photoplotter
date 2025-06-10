50902c1e6c6df04140dbe0376ebdd358403aad30
Philipp Lehmann <philipp@redefine.studio>
2025-06-10 18:29:12 +0200

    Smaller depth map

--- photobooth/imageparser.py
+++ photobooth/imageparser.py
@@ -42,7 +42,7 @@ class ImageParser:
 
         self.transform = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Resize((256, 256)),
+            transforms.Resize((192, 192)),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
     
