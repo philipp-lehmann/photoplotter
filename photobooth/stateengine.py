a76c515831fdd31c853372661829adf98f3df674
Philipp Lehmann <philipp@redefine.studio>
2025-10-11 12:52:18 +0200

    Make sure position one is last

--- photobooth/stateengine.py
+++ photobooth/stateengine.py
@@ -92,7 +92,8 @@ class StateEngine:
 
     def reset_photo_id(self):
         triplets = [
-            [1, 2, 3, 4, 5],
+            [1],
+            [2, 3, 4, 5],
             [6, 7, 8, 9, 10],
             [11, 12, 13, 14, 15]
         ]
